da96a474cde5d9123eeee9fe1d05415096907d11
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-07 19:22:12 -0400

    Whitespace fixes

--- lua/autorun/client/cl_cfc_performance.lua
+++ lua/autorun/client/cl_cfc_performance.lua
@@ -1,44 +1,44 @@
 AddCSLuaFile()
 
 local function removeHooks()
-    hook.Remove("GUIMousePressed", "SuperDOFMouseDown")
-    hook.Remove("GUIMouseReleased", "SuperDOFMouseUp")
-    hook.Remove("PreventScreenClicks", "SuperDOFPreventClicks")
-    hook.Remove("PostRender", "RenderFrameBlend")
-    hook.Remove("PreRender", "PreRenderFrameBlend")
-    hook.Remove("PostDrawEffects", "RenderWidgets")
-    --hook.Remove("PostDrawEffects", "RenderHalos")
-    hook.Remove("RenderScene", "RenderStereoscopy")
-    hook.Remove("RenderScene", "RenderSuperDoF")
-    hook.Remove("RenderScreenspaceEffects", "RenderBloom")
-    hook.Remove("RenderScreenspaceEffects", "RenderBokeh")
-    hook.Remove("RenderScreenspaceEffects", "RenderColorModify")
-    hook.Remove("RenderScreenspaceEffects", "RenderMaterialOverlay")
-    hook.Remove("RenderScreenspaceEffects", "RenderMotionBlur")
-    hook.Remove("RenderScreenspaceEffects", "RenderSharpen")
-    hook.Remove("RenderScreenspaceEffects", "RenderSobel")
-    hook.Remove("RenderScreenspaceEffects", "RenderSunbeams")
-    hook.Remove("RenderScreenspaceEffects", "RenderTexturize")
-    hook.Remove("RenderScreenspaceEffects", "RenderToyTown")
-    hook.Remove("Think", "DOFThink")
-    hook.Remove("NeedsDepthPass", "NeedsDepthPass_Bokeh")
+    hook.Remove( "GUIMousePressed", "SuperDOFMouseDown" )
+    hook.Remove( "GUIMouseReleased", "SuperDOFMouseUp" )
+    hook.Remove( "PreventScreenClicks", "SuperDOFPreventClicks" )
+    hook.Remove( "PostRender", "RenderFrameBlend" )
+    hook.Remove( "PreRender", "PreRenderFrameBlend" )
+    hook.Remove( "PostDrawEffects", "RenderWidgets" )
+    -- hook.Remove( "PostDrawEffects", "RenderHalos" )
+    hook.Remove( "RenderScene", "RenderStereoscopy" )
+    hook.Remove( "RenderScene", "RenderSuperDoF" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderBloom" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderBokeh" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderColorModify" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderMaterialOverlay" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderMotionBlur" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderSharpen" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderSobel" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderSunbeams" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderTexturize" )
+    hook.Remove( "RenderScreenspaceEffects", "RenderToyTown" )
+    hook.Remove( "Think", "DOFThink" )
+    hook.Remove( "NeedsDepthPass", "NeedsDepthPass_Bokeh" )
 end
 
 local function runCommands()
-    RunConsoleCommand("cl_threaded_bone_setup", "1")
-    RunConsoleCommand("cl_threaded_client_leaf_system", "1")
-    RunConsoleCommand("gmod_mcore_test", "1") -- It was suggested to set this value to -2 for some reason
-    RunConsoleCommand("mat_queue_mode", "-2")
-    RunConsoleCommand("r_decals", "25") -- Max Decals
-    RunConsoleCommand("r_queued_decals", "1")-- potentially unstable
-    RunConsoleCommand("r_queued_ropes", "1") -- potentially unstable
-    RunConsoleCommand("r_queued_post_processing", "1")
-    RunConsoleCommand("r_threaded_client_shadow_manager", "1")
-    RunConsoleCommand("r_threaded_particles", "1")
-    RunConsoleCommand("r_threaded_renderables", "1")
-    RunConsoleCommand("studio_queue_mode", "1")
-    RunConsoleCommand("prop_active_gib_limit", "0")
-    RunConsoleCommand("snd_mix_async", "1") --Multithreaded sound. Causes the first few milliseconds of sounds to be cut off on some systems.
+    RunConsoleCommand( "cl_threaded_bone_setup", "1" )
+    RunConsoleCommand( "cl_threaded_client_leaf_system", "1" )
+    RunConsoleCommand( "gmod_mcore_test", "1" ) -- It was suggested to set this value to -2 for some reason
+    RunConsoleCommand( "mat_queue_mode", "-2" )
+    RunConsoleCommand( "r_decals", "25" ) -- Max Decals
+    RunConsoleCommand( "r_queued_decals", "1" )-- potentially unstable
+    RunConsoleCommand( "r_queued_ropes", "1" ) -- potentially unstable
+    RunConsoleCommand( "r_queued_post_processing", "1" )
+    RunConsoleCommand( "r_threaded_client_shadow_manager", "1" )
+    RunConsoleCommand( "r_threaded_particles", "1" )
+    RunConsoleCommand( "r_threaded_renderables", "1" )
+    RunConsoleCommand( "studio_queue_mode", "1" )
+    RunConsoleCommand( "prop_active_gib_limit", "0" )
+    RunConsoleCommand( "snd_mix_async", "1" ) -- Multithreaded sound. Causes the first few milliseconds of sounds to be cut off on some systems.
 end
 
 local function disableWidgets()
@@ -55,10 +55,10 @@ local function warnEnable( ply, message, source )
 
     log( "[CFC Enhancements] Warning! This command may cause unintended side effects, such as stuttering or crashing" )
     log( "[CFC Enhancements] If you experience any issues, please simply rejoin the server" )
-    log( "[CFC Enhancements] Enabling in two seconds.." )
+    log( "[CFC Enhancements] Enabling in two seconds .. " )
 
     timer.Simple( 2, function()
-        log( "[CFC Enhancements] Enabling CFC Performance Enhancements.." )
+        log( "[CFC Enhancements] Enabling CFC Performance Enhancements .. " )
         enableEnhancements()
         log( "[CFC Enhancements] CFC Performance Enhancements enabled! Enjoy enhanced performance!" )
     end )
